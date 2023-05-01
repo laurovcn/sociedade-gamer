54f0470543120b5c2172aec9ee084d574f221631
lvneto <lauro.neto.1995@gmail.com>
2023-05-01 13:38:27 -0300

    add eslint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,32 @@
-import { signIn, signOut, useSession } from "next-auth/react";
-import Image from "next/image";
+import { signIn, signOut, useSession } from 'next-auth/react'
+import Image from 'next/image'
 
 export default function Home() {
-  const { data: session } = useSession()    
-  
+  const { data: session } = useSession()
+
   if (session) {
     return (
       <>
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">     
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
           <h1>{session?.user?.email}</h1>
           <h1>{session?.user?.name}</h1>
-          <Image alt="avatar do usuário" width={200} height={200} src={session?.user?.image!}/>
+          <Image
+            alt="avatar do usuário"
+            width={200}
+            height={200}
+            src={session?.user?.image!}
+          />
           <button onClick={() => signOut()}>Deslogar</button>
-        </main>   
+        </main>
       </>
     )
   }
-  return (  
+  return (
     <>
-     <main className="flex min-h-screen flex-col items-center justify-between p-24">  
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
         Você não está logado. <br />
         <button onClick={() => signIn()}>Logar</button>
-      </main>  
+      </main>
     </>
   )
 }
